38466c6f931ac8ca0c67c98fd828fc9f3fe0904a
Sudaraka Soysa <Sudaraka_105272@dialog.dialoggsm.com>
2020-07-26 20:06:44 +0530

    comment

---
+++ src/main/java/com/adl/sample/sampleproject/TestDto.java
@@ -0,0 +1,23 @@
+package com.adl.sample.sampleproject;
+
+public class TestDto {
+    private String id;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    private String message;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
